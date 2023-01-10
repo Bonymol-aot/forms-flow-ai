0a910313c5038b7298c23517c7e976dec02b52de
Bonymol-aot <bonymol@aot-technologies.com>
2023-01-10 13:26:21 +0530

    Added Internationalizataion for Form Versioning updates

--- forms-flow-web/src/components/Form/Item/SaveAsNewVersionConfirmationModal.js
+++ forms-flow-web/src/components/Form/Item/SaveAsNewVersionConfirmationModal.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 const SaveAsNewVersionConfirmationModal = ({modalOpen,handleModalChange,onConfirm}) => {
+  const { t } = useTranslation();
   return (
     <Modal
     show={modalOpen}
@@ -11,7 +13,7 @@ const SaveAsNewVersionConfirmationModal = ({modalOpen,handleModalChange,onConfir
     <Modal.Header>
       <div>
         <Modal.Title id="example-custom-modal-styling-title">
-          Save as a new version
+          {t("Save as a new version")}
         </Modal.Title>
       </div>
 
@@ -33,16 +35,15 @@ const SaveAsNewVersionConfirmationModal = ({modalOpen,handleModalChange,onConfir
       <div className="d-flex align-items-start p-3">
         <i className="fa fa-info-circle text-primary mr-2"></i>
         <span > 
-        This will create a new version of the form without changing 
-        the path name but with new form id.
+        {t("This will create a new version of the form without changing the path name but with new form id.")}
         </span>
       </div>
 
     </Modal.Body>
     <Modal.Footer>
       <div className='d-flex justify-content-end'>
-          <button className='btn btn-danger mr-2' onClick={()=>{handleModalChange();}}>Cancel</button>
-          <button className='btn btn-primary' onClick={()=>{onConfirm();}}>Continue</button>
+          <button className='btn btn-danger mr-2' onClick={()=>{handleModalChange();}}>{t("Cancel")}</button>
+          <button className='btn btn-primary' onClick={()=>{onConfirm();}}>{t("Continue")}</button>
       </div>
     </Modal.Footer>
   </Modal>
